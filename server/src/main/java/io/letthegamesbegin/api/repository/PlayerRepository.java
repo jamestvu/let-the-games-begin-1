2f201c137c58cfa4fe3f1c786128d57db25ef99c
James Vu <jamestvu@users.noreply.github.com>
2020-03-01 01:23:13 -1000

    Removed comments.

--- server/src/main/java/io/letthegamesbegin/api/repository/PlayerRepository.java
+++ server/src/main/java/io/letthegamesbegin/api/repository/PlayerRepository.java
@@ -4,9 +4,6 @@ import io.letthegamesbegin.api.bracket.Player;
 import io.letthegamesbegin.api.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called UserRepository
-// CRUD refers Create, Read, Update, Delete
-
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
 
 }
