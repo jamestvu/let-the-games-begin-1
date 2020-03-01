3505ee6c9f78954a0f44a8acdd8859060fb1e67b
David Do <david@daviddo.dev>
2020-02-29 17:43:31 -1000

    Spring backend API

---
+++ server/src/main/java/io/letthegamesbegin/api/LetthegamesbeginApplication.java
@@ -0,0 +1,13 @@
+package io.letthegamesbegin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetthegamesbeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetthegamesbeginApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/letthegamesbegin/api/LetthegamesbeginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.letthegamesbegin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetthegamesbeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
